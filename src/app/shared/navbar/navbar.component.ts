ea7cd6e318cd6072885d81baac4f7834fb94ef81
planimir77 <planimir7708@gmail.com>
2020-11-30 10:28:00 +0200

    refactor: delete unused code

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -10,8 +9,7 @@ import { Input } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  @Input()
-  ngStyle: { [klass: string]: any; }
+
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
