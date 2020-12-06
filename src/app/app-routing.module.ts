2947dff5a4247efcd400a1715029cfb2ae8aec6f
planimir77 <planimir7708@gmail.com>
2020-12-06 11:45:05 +0200

    feat: add user module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ShoppinglistCreateComponent } from './shoppinglist/shoppinglist-create/shoppinglist-create.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'shopping-list-create', component: ShoppinglistCreateComponent},
-  {path: '', redirectTo: 'home', pathMatch: 'full'}
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'shoppinglist', loadChildren: () => import('./shoppinglist/shoppinglist.module').then(m => m.ShoppinglistModule) },
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
 ]
 
 @NgModule({
