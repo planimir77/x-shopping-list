c42acbd55be98b3ab79dba990ff6afff19da51b7
planimir77 <planimir7708@gmail.com>
2020-12-08 12:52:50 +0200

    feat: add user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { UserRoutingModule } from './user-routing.module';
 import { MaterialModule } from '../material/material.module'
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
+import { ValidateEqualModule } from 'ng-validate-equal';
+
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [
+    LoginComponent, 
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
     UserRoutingModule,
     MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ValidateEqualModule
   ]
 })
 export class UserModule { }
