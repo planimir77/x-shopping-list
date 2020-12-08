efeda0de35bb22c8efb3d1496c4f73c6a403acb9
planimir77 <planimir7708@gmail.com>
2020-12-08 13:03:28 +0200

    refactor: done user register

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,14 +10,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  test: Date = new Date();
   focus;
   focus1;
   focus2;
 
-  constructor() { }
+  form: FormGroup;
+  isLoading = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(20)
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+      rePassword: new FormControl('', [
+        Validators.required,
+      ]),
+        
+    });
+  }
+
+  public checkError = (controlName: string, errorName: string) => {
+    if (this.form.touched) {
+
+      return this.form.controls[controlName].hasError(errorName);
+    }
+  }
+  get username() { return this.form.get('username') };
+  get email() { return this.form.get('email') };
+  get password() { return this.form.get('password') };
+  get rePassword() { return this.form.get('rePassword') };
+
+  onSubmit(): void {
+    const data = this.form.value;
+    this.isLoading = true;
+
+    this.authService.register(data).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.isLoading = false;
+        this.router.navigate(['/user/login']);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.log(err);
+      }
+    });
   }
 
 }
