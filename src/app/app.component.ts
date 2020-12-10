b34c13935d4ab0da4d40af0f86f7ea94d001e285
planimir77 <planimir7708@gmail.com>
2020-12-10 09:47:51 +0200

    refactor: add navbar to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,38 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from './core/services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  currentUser ;
+  isLogged$ = this.authService.isLogged$;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
     
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    private authService: AuthService,
+    ) {}
+
+  ngOnInit(): void {
+    this.authService.currentUser$.subscribe(currentUser => 
+      this.currentUser = currentUser);
+  }
+
+  logout(): void {
+    this.authService.logout().subscribe(() => this.router.navigate(['/']));
+  }
 
 }
