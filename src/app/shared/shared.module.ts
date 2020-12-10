9192fd6f69f003362ff5ccca6839f7f21edb1863
planimir77 <planimir7708@gmail.com>
2020-12-10 20:37:49 +0200

    feat: add loading component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
+import { LoadingComponent } from './loading/loading.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
 @NgModule({
   declarations: [
-    CapitalizePipe
+    CapitalizePipe,
+    LoadingComponent
   ],
   imports: [
     CommonModule,
+    MaterialModule,
   ],
   exports: [
-    CapitalizePipe
-  ]
+    CapitalizePipe,
+    LoadingComponent
+  ],
 })
 export class SharedModule { }
