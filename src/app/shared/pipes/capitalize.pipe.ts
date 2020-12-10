82cba14d53a4276b4a757edc7b63fbff7b3e9a11
planimir77 <planimir7708@gmail.com>
2020-12-10 09:49:05 +0200

    feat: add capitalize pipe

---
+++ src/app/shared/pipes/capitalize.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'capitalize'
+})
+export class CapitalizePipe implements PipeTransform {
+
+  transform(word: string): string {
+    return word[0].toUpperCase() + word.substr(1).toLowerCase() + "'s";
+  }
+
+}
