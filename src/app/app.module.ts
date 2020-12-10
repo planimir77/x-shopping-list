bc3e153e2dd5197d32221d2916538c1e3e9c3026
planimir77 <planimir7708@gmail.com>
2020-12-10 16:24:43 +0200

    refactor: remove redundant providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,6 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     SharedModule
   ],
-  providers: [appInterceptorProvider, AuthGuard],
   bootstrap: [
     AppComponent,
   ],
