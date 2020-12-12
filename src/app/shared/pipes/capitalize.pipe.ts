3944bdd200a51a137034b2cd5b29ddc4e6c2d1fa
planimir77 <planimir7708@gmail.com>
2020-12-12 21:26:06 +0200

    fix: input is null

--- src/app/shared/pipes/capitalize.pipe.ts
+++ src/app/shared/pipes/capitalize.pipe.ts
@@ -6,7 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CapitalizePipe implements PipeTransform {
 
   transform(word: string): string {
-    return word[0].toUpperCase() + word.substr(1).toLowerCase();
+    if (word){
+      return word[0].toUpperCase() + word.substr(1).toLowerCase();
+    }
+    return null;
   }
 
 }
