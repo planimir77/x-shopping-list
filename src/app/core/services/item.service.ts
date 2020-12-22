2c8d7e481caf4bb31ee6bccee2b591968c945a4e
planimir77 <planimir7708@gmail.com>
2020-12-22 13:11:06 +0200

    feat: add services add ,remove ItemSubscribers

--- src/app/core/services/item.service.ts
+++ src/app/core/services/item.service.ts
@@ -12,22 +12,36 @@ export class ItemService {
   constructor(private httpClient: HttpClient) { }
 
   // post("/api/item/create")
-  createItem(itemName: string, shoppinglistId: string ): Observable<IItem> {
+  createItem(itemName: string, shoppinglistId: string): Observable<IItem> {
     return this.httpClient.post<IItem>(
-        `${apiUrl}/api/item/create`,
-        {itemName, shoppinglistId},
-      );
+      `${apiUrl}/api/item/create`,
+      { itemName, shoppinglistId },
+    );
+  }
+  // post("/api/item/add")
+  addItemSubscribers(itemId: string, shoppinglistId: string) {
+    return this.httpClient.post<IItem>(
+      `${apiUrl}/api/item/add`,
+      { itemId, shoppinglistId },
+    );
+  }
+  // post("/api/item/remove")
+  removeItemSubscribers(itemId: string, shoppinglistId: string) {
+    return this.httpClient.post<IItem>(
+      `${apiUrl}/api/item/remove`,
+      { itemId, shoppinglistId },
+    );
   }
   // get("/api/item/:itemId&shoppinglistId")
-  loadItem(_id: string, shoppinglistId: string): Observable<IItem> {
+  loadItem(_id: string): Observable<IItem> {
     return this.httpClient.get<IItem>(
-      `${apiUrl}/api/item/${_id}&${shoppinglistId}`
-      );
+      `${apiUrl}/api/item/${_id}`
+    );
   }
   // get("/api/item/:itemId&shoppinglistId")
-  loadItemByName(itemName: string, shoppinglistId: string): Observable<IItem> {
+  loadItemByName(itemName: string): Observable<IItem> {
     return this.httpClient.get<IItem>(
-      `${apiUrl}/api/item/by-name/${itemName}&${shoppinglistId}`
-      );
+      `${apiUrl}/api/item/by-name/${itemName}`
+    );
   }
 }
