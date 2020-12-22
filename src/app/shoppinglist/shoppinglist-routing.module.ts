a972acbadc3c9a7a64951be820f1c952612199de
planimir77 <planimir7708@gmail.com>
2020-12-22 13:21:04 +0200

    fix: add route rules "isLogged"

--- src/app/shoppinglist/shoppinglist-routing.module.ts
+++ src/app/shoppinglist/shoppinglist-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {  ShoppinglistCreateComponent } from './create-component/shoppinglist-create.component';
+import { ShoppinglistDashboardComponent } from './dashboard-component/shoppinglist-dashboard.component';
 import { ShoppinglistDetailsComponent } from './details-component/shoppinglist-details.component';
 
 const routes: Routes = [
-  { path: 'create', component: ShoppinglistCreateComponent },
-  { path: ':id', component: ShoppinglistDetailsComponent}
+  { path: 'create', component: ShoppinglistCreateComponent, data: {isLogged: true,}},
+  { path: 'dashboard', component: ShoppinglistDashboardComponent, data: {isLogged: true,}},
+  { path: ':id', component: ShoppinglistDetailsComponent, data: {isLogged: false,}},
 ];
 
-@NgModule({
+@NgModule({ 
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -5,8 +5,8 @@ import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'login', component: LoginComponent, data: {isLogged: false,}},
+  {path: 'register', component: RegisterComponent, data: {isLogged: false,}}
 ];
 
 @NgModule({
