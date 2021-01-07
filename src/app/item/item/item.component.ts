9888001f87de1b4d511c58733b0aec8fa8e15651
planimir77 <planimir7708@gmail.com>
2021-01-07 14:28:35 +0200

    refactor: add EventEmitter

--- src/app/item/item/item.component.ts
+++ src/app/item/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { ItemService } from 'src/app/core/services';
 import { IItem, IShoppinglist } from 'src/app/shared/interfaces';
 
@@ -11,42 +12,15 @@ export class ItemComponent implements OnInit {
 
   @Input() item: IItem;
   @Input() shoppinglist: IShoppinglist;
+  @Output() itemChange = new EventEmitter<string>();
   isInBasket: boolean = false;
 
-  constructor(private itemService: ItemService) { }
+  constructor() { }
 
   ngOnInit(): void {
     this.isInBasket = this.item.subscribers.some(sub => sub === this.shoppinglist?._id);
   }
-
   addToBasket(itemId: string) {
-    this.itemService.loadItem(itemId).subscribe(item => {
-      const subscriberId = item.subscribers.find(sub => sub === this.shoppinglist?._id);
-      if (subscriberId) {
-        this.itemService.removeItemSubscribers(itemId, subscriberId).subscribe({
-          next: (response) => {
-            this.item.subscribers.splice(this.item.subscribers.indexOf(subscriberId), 1);
-            this.isInBasket = false;
-          },
-          error: (err) => {
-            //this.isLoading = false;
-            console.log(err);
-          }
-        })
-      } else {
-        this.itemService.addItemSubscribers(itemId, this.shoppinglist._id).subscribe({
-          next: (response) => {
-            this.item.subscribers.push(this.shoppinglist._id);
-            this.isInBasket = true;
-          },
-          error: (err) => {
-            //this.isLoading = false;
-            console.log(err);
-          }
-        })
-      }
-    });
+    this.itemChange.emit(itemId);
   }
-
-
 }
