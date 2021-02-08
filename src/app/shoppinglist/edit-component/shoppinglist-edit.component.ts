6f0bd9a3322aeeab661e792e9b809980f3864101
planimir77 <planimir7708@gmail.com>
2021-02-08 19:52:17 +0200

    feat: create edit component

---
+++ src/app/shoppinglist/edit-component/shoppinglist-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppinglist-edit',
+  templateUrl: './shoppinglist-edit.component.html',
+  styleUrls: ['./shoppinglist-edit.component.scss']
+})
+export class ShoppinglistEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
