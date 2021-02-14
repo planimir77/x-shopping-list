109243ed342851b3775150c00165683554e32204
planimir77 <planimir7708@gmail.com>
2021-02-14 17:47:06 +0200

    refactor:"item-subscribed-list component" done

--- src/app/item/item-subscribed-list/item-subscribed-list.component.ts
+++ src/app/item/item-subscribed-list/item-subscribed-list.component.ts
@@ -1,19 +1,55 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { IShoppinglist } from 'src/app/shared/interfaces';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ItemService } from 'src/app/core/services';
+import { IItem } from 'src/app/shared/interfaces';
+import { NO_ITEMS_YET, NO_CHECKED_ITEMS, DONE} from 'src/app/shared/constants';
 
 @Component({
   selector: 'app-item-subscribed-list',
   templateUrl: './item-subscribed-list.component.html',
-  styleUrls: ['./item-subscribed-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.Default   
+  styleUrls: ['./item-subscribed-list.component.scss']
 })
 export class ItemSubscribedListComponent implements OnInit {
 
-  @Input() shoppinglist: IShoppinglist;
+  @Input() shoppinglistId: string;
+  @Input() items: IItem[];
+  @Input() isDone: boolean;
+  @Input() subscribedItems: boolean;
+  @Output() itemChange = new EventEmitter<IItem>();
+  noItemsYet: string;
+  noCheckedItems: string;
+  done: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private itemService: ItemService) {
+    this.noItemsYet = NO_ITEMS_YET;
+    this.noCheckedItems = NO_CHECKED_ITEMS;
+    this.done = DONE;
   }
 
+  ngOnInit(): void { }
+
+  setStyle(item: any):any { 
+    if(item.subscribers.includes(this.shoppinglistId)){
+      return { display: 'block' };
+    }else{
+      return { display: 'none' };
+    }
+  }
+
+  isNoItems():boolean {
+    debugger;
+    return this.items?.length === 0;
+  }
+
+  unsubscribe(item: IItem){ 
+    this.itemService.unsubscribe(item._id, this.shoppinglistId).subscribe({
+      next: (response) => {
+        this.itemChange.emit(response);
+      },
+      error: (err) => {
+        throw new Error("Not implemented");
+        //this.isLoading = false;
+        //this.errorHandler(err);
+      }
+    });
+  }
 }
