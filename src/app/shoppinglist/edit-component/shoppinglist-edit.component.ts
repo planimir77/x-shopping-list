4868d3ed56c8131c7dece4e3220646125fbae13c
planimir77 <planimir7708@gmail.com>
2021-02-14 18:05:07 +0200

    refactor: done create component

--- src/app/shoppinglist/edit-component/shoppinglist-edit.component.ts
+++ src/app/shoppinglist/edit-component/shoppinglist-edit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  id:string,
+  name: string,
+  index:number,
+}
 
 @Component({
   selector: 'app-shoppinglist-edit',
@@ -7,9 +14,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppinglistEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<ShoppinglistEditComponent>,
+    @Inject(MAT_DIALOG_DATA) 
+    public data: DialogData,
+    ) { }
 
   ngOnInit(): void {
   }
 
+  onCancelClick(): void {
+    this.dialogRef.close();
+  }
 }
