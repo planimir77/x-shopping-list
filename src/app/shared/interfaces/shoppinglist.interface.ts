af10c4a44b810f903647784feda6dd685d6e2e40
planimir77 <planimir7708@gmail.com>
2021-02-16 18:32:55 +0200

    feat: add favorite property

--- src/app/shared/interfaces/shoppinglist.interface.ts
+++ src/app/shared/interfaces/shoppinglist.interface.ts
@@ -3,6 +3,7 @@ import { IUser, IBase, IItem } from '.';
 export interface IShoppinglist extends IBase {
     [x: string]: any;
     shoppinglistName: string,
+    favorite: boolean,
     subscribers: IUser[],
     items: IItem[],
     userId: IUser,
